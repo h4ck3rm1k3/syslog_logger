7a0f012998de90f1f0952d87f7819f6a3fe8b7fc
james mike dupont <jamesmikedupont@gmail.com>
2016-07-03 14:30:39 -0400

    update mysyslog with file

--- lib/mysyslog.rb
+++ lib/mysyslog.rb
@@ -23,4 +23,36 @@ end
 class LogDevice
 end
 
+#class File
+# end
+module FileTest
+
+  #FileTest.blockdev?(file_name)
+#                 FileTest.chardev?(file_name)
+#                 FileTest.directory?(file_name)
+#                 FileTest.executable?(file_name)
+#                 FileTest.executable_real?(file_name)
+  def FileTest.exist? file_name
+    return false
+  end
+  #alias exists? exist?
+#                 FileTest.file?(file_name)
+#                 FileTest.grpowned?(file_name)
+#                 FileTest.identical?(file_1, file_2)
+#                 FileTest.owned?(file_name)             -- do I own file?
+#                 FileTest.pipe?(file_name)
+#                 FileTest.readable?(file_name)
+#                 FileTest.readable_real?(file_name)
+#                 FileTest.setgid?(file_name)
+#                 FileTest.setuid?(file_name)
+#                 FileTest.size(file_name)
+#                 FileTest.size?(file_name)              -- nil if missing, integer other wise
+#                 FileTest.socket?(file_name)
+#                 FileTest.sticky?(file_name)
+#                 FileTest.symlink?(file_name)
+#                 FileTest.writable?(file_name)
+#                 FileTest.writable_real?(file_name)
+  #                 FileTest.zero?(file_name)
+  
+end
 
